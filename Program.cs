42026114e3d68f6c068679ffd39176f5a7546ebd
Yosmel Chiang <yosmelchiang@gmail.com>
2026-02-14 21:57:06 +0100

    extend on generic delegates

--- Program.cs
+++ Program.cs
@@ -15,10 +15,19 @@
         // In this example we are using it to declare an anonymous function
         public delegate string MyGreeting(string s);
 
+        // This delegate takes two values of type T and returns a value of type T
+        public delegate T Calculator<T>(T num1, T num2);
+
+        // This is a generic methodn that takes a generic delegate
+        // This method returns type T and takes two parameters of type T as well as a generic delegate
+        public static T Operate<T>(T a, T b, Calculator<T> operation)
+        {
+            return operation(a, b);
+        }
         public static void Main()
         {
             // Using a delegate with an instance of a class
-            Dog myPet = new Dog();
+            Dog myPet = new Dog("Ella", 4);
             myPet.makeSound("barks"); // Calling makeSound directly from the class
 
             MyDelegate del = myPet.makeSound;
@@ -56,10 +65,41 @@
             Console.WriteLine(isEven(5)); // False
             Console.WriteLine(isEven(4)); // True
             sum(5, 10); // 15
+
+            // Generic Delegates simply specifies that it can return any type, and take any type of paramters
+            // The logic of the method passed to a generic delegate must support the types
+            // addInt works as long as the arguments passed are ints.
+            // addDouble works as long as the arguments passed are doubles.
+            // Essensitally, the Calculator delegate can be replaced by Func<T, T, T>, because they achieve the same thing in this example
+            // Meaning we can just replace it with Func<int, int, int> addInt = (a, b) => a + b; instead
+            Calculator<int> addInt = (a, b) => a + b; // takes two ints and returns int
+            Calculator<double> addDouble = (a, b) => a + b; // same as above, but returns a double type
+            Calculator<string> concat = (a, b) => a + b; // takes a string and returns a string
+            Calculator<Dog> compareDogs = (dog1, dog2) => dog1.Age > dog2.Age ? dog1 : dog2; // takes two dog instances and returns a dog instance
+
+            Dog oldest = compareDogs(new Dog("Rex", 5), new Dog("Buddy", 3));
+
+            Console.WriteLine(addInt(4, 4)); // 8
+            Console.WriteLine(addDouble(2.3, 3.2)); // 5.5
+            Console.WriteLine(concat("2", "2")); // 22
+            Console.WriteLine(oldest.Name + " is " + oldest.Age + " years old and is oldest.");
+
+            // Generic Method that takes a Generic Delegate
+            // A more powerful way of using generic
+            Console.WriteLine(Operate(50, 50, addInt)); // 100
+            Console.WriteLine(Operate("Hello ", "World", concat));
         }
     }
     class Dog
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Dog(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
         public void makeSound(string sound)
         {
             Console.WriteLine(sound);
